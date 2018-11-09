098f5e3bed2e630c3701684b257c8b67b86cc37e
Lewis <contact@artoria.us>
2018-11-09 19:48:13 +0900

    I AM PROFESSIONAL SPAGHETTI MAKER

--- sanic_jwt_extended/__init__.py
+++
@@ -1,7 +0,0 @@
-from .jwt_manager import JWTManager
-from .view_decorators import (
-    fresh_jwt_required, jwt_optional, jwt_refresh_token_required, jwt_required,
-    verify_fresh_jwt_in_request, verify_jwt_in_request,
-    verify_jwt_in_request_optional, verify_jwt_refresh_token_in_request
-)
-__version__ = '0.0.1'

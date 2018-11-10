10ace39ce3b5f8f8ba5e641e663f8ef1d0ef80d5
Lewis <contact@artoria.us>
2018-11-10 22:29:14 +0900

    ADD: exception classes

---
+++ sanic_jwt_extended/exceptions.py
@@ -0,0 +1,49 @@
+class JWTExtendedException(Exception):
+    """
+    Base except which all flask_jwt_extended errors extend
+    """
+    pass
+
+
+class JWTDecodeError(JWTExtendedException):
+    """
+    An error decoding a JWT
+    """
+    pass
+
+
+class InvalidHeaderError(JWTExtendedException):
+    """
+    An error getting header information from a request
+    """
+    pass
+
+
+class NoAuthorizationError(JWTExtendedException):
+    """
+    An error raised when no authorization token was found in a protected endpoint
+    """
+    pass
+
+
+class WrongTokenError(JWTExtendedException):
+    """
+    Error raised when attempting to use a refresh token to access an endpoint
+    or vice versa
+    """
+    pass
+
+
+class RevokedTokenError(JWTExtendedException):
+    """
+    Error raised when a revoked token attempt to access a protected endpoint
+    """
+    pass
+
+
+class FreshTokenRequired(JWTExtendedException):
+    """
+    Error raised when a valid, non-fresh JWT attempt to access an endpoint
+    protected by fresh_jwt_required
+    """
+    pass

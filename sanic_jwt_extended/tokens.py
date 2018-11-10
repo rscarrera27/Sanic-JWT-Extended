565eecde863ad239f9537e6ab3ce2d505f6703e6
Lewis <contact@artoria.us>
2018-11-10 22:34:07 +0900

    UPDATE: token encoder

--- sanic_jwt_extended/tokens.py
+++ sanic_jwt_extended/tokens.py
@@ -26,3 +26,58 @@ def _encode_jwt(additional_token_data, expires_delta, secret, algorithm,
                                json_encoder=json_encoder).decode('utf-8')
     return encoded_token
 
+
+def encode_access_token(identity, secret, algorithm, expires_delta, fresh,
+                        user_claims, identity_claim_key, user_claims_key,
+                        json_encoder=None):
+
+    if isinstance(fresh, datetime.timedelta):
+        now = datetime.datetime.utcnow()
+        fresh = timegm((now + fresh).utctimetuple())
+
+    token_data = {
+        identity_claim_key: identity,
+        'fresh': fresh,
+        'type': 'access',
+    }
+
+    # Don't add extra data to the token if user_claims is empty.
+    if user_claims:
+        token_data[user_claims_key] = user_claims
+
+    return _encode_jwt(token_data, expires_delta, secret, algorithm,
+                       json_encoder=json_encoder)
+
+
+def encode_refresh_token(identity, secret, algorithm, expires_delta, user_claims,
+                         identity_claim_key, user_claims_key,
+                         json_encoder=None):
+    """
+    Creates a new encoded (utf-8) refresh token.
+
+    :param identity: Some identifier used to identify the owner of this token
+    :param secret: Secret key to encode the JWT with
+    :param algorithm: Which algorithm to use for the toek
+    :param expires_delta: How far in the future this token should expire
+                          (set to False to disable expiration)
+    :type expires_delta: datetime.timedelta or False
+    :param user_claims: Custom claims to include in this token. This data must
+                        be json serializable
+    :param csrf: Whether to include a csrf double submit claim in this token
+                 (boolean)
+    :param identity_claim_key: Which key should be used to store the identity
+    :param user_claims_key: Which key should be used to store the user claims
+    :return: Encoded refresh token
+    """
+    token_data = {
+        identity_claim_key: identity,
+        'type': 'refresh',
+    }
+
+    # Don't add extra data to the token if user_claims is empty.
+    if user_claims:
+        token_data[user_claims_key] = user_claims
+
+    return _encode_jwt(token_data, expires_delta, secret, algorithm,
+                       json_encoder=json_encoder)
+

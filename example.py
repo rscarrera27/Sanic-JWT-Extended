63268b0569805578bd7d8fff015fd35f4d5cb955
Lewis <contact@artoria.us>
2018-11-11 11:48:45 +0900

    UPDATE: asynchronus example

--- example.py
+++ example.py
@@ -3,7 +3,7 @@ from sanic.response import json
 from sanic.request import Request
 from sanic_jwt_extended import (
     JWTManager, jwt_required, create_access_token,
-    Token)
+    Token, create_refresh_token, uuid)
 
 app = Sanic(__name__)
 
@@ -16,7 +16,7 @@ JWTManager(app)
 # function is used to actually generate the token, and you can return
 # it to the caller however you choose.
 @app.route('/login', methods=['POST'])
-def login(request: Request):
+async def login(request: Request):
     if not request.json:
         return json({"msg": "Missing JSON in request"}, status=400)
 
@@ -31,15 +31,18 @@ def login(request: Request):
         return json({"msg": "Bad username or password"}, status=403)
 
     # Identity can be any data that is json serializable
-    access_token = create_access_token(identity=username, app=request.app)
-    return json(dict(access_token=access_token), status=200)
+    access_token = await create_access_token(identity=username, app=request.app)
+    refresh_token = await create_refresh_token(identity=str(uuid.uuid4()), app=request.app)
+    return json(dict(access_token=access_token,
+                     refresh_token=refresh_token
+                     ), status=200)
 
 
 # Protect a view with jwt_required, which requires a valid access token
 # in the request to access.
 @app.route('/protected', methods=['GET'])
 @jwt_required
-def protected(request: Request, token: Token):
+async def protected(request: Request, token: Token):
     # Access the identity of the current user with get_jwt_identity
     current_user = token.raw_jwt
     return json(dict(data=current_user))

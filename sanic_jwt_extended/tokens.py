9950cec713e993ce95973bc3a6bc309a23e98b79
Lewis <contact@artoria.us>
2018-11-12 19:51:01 +0900

    UPDATE: docstring

--- sanic_jwt_extended/tokens.py
+++ sanic_jwt_extended/tokens.py
@@ -2,14 +2,17 @@ import datetime
 import uuid
 
 from calendar import timegm
+from typing import Union, Dict, Callable
 
 import jwt
 
 from flask_jwt_extended.exceptions import JWTDecodeError
+from sanic import Sanic
 
 
-def _encode_jwt(additional_token_data, expires_delta, secret, algorithm,
-                json_encoder=None):
+def _encode_jwt(additional_token_data: dict, expires_delta: datetime.timedelta, secret: str, algorithm: str,
+                json_encoder: Callable[..., str]) -> str:
+
     uid = str(uuid.uuid4())
     now = datetime.datetime.utcnow()
     token_data = {
@@ -27,9 +30,29 @@ def _encode_jwt(additional_token_data, expires_delta, secret, algorithm,
     return encoded_token
 
 
-async def encode_access_token(identity, secret, algorithm, expires_delta, fresh,
-                              user_claims, identity_claim_key, user_claims_key,
-                              json_encoder=None):
+async def encode_access_token(identity: str, secret: str, algorithm: str, expires_delta: datetime.timedelta,
+                              fresh: Union[datetime.timedelta, bool],
+                              user_claims: dict, identity_claim_key: str, user_claims_key: str,
+                              json_encoder: Callable[..., str]=None) -> str:
+    """
+    Creates a new encoded (utf-8) access token.
+    :param identity: Identifier for who this token is for (ex, username). This
+                     data must be json serializable
+    :param secret: Secret key to encode the JWT with
+    :param algorithm: Which algorithm to encode this JWT with
+    :param expires_delta: How far in the future this token should expire
+                          (set to False to disable expiration)
+    :type expires_delta: datetime.timedelta or False
+    :param fresh: If this should be a 'fresh' token or not. If a
+                  datetime.timedelta is given this will indicate how long this
+                  token will remain fresh.
+    :param user_claims: Custom claims to include in this token. This data must
+                        be json serializable
+    :param identity_claim_key: Which key should be used to store the identity
+    :param user_claims_key: Which key should be used to store the user claims
+    :param json_encoder: json encoder
+    :return: Encoded access token
+    """
     if isinstance(fresh, datetime.timedelta):
         now = datetime.datetime.utcnow()
         fresh = timegm((now + fresh).utctimetuple())
@@ -62,10 +85,9 @@ async def encode_refresh_token(identity, secret, algorithm, expires_delta, user_
     :type expires_delta: datetime.timedelta or False
     :param user_claims: Custom claims to include in this token. This data must
                         be json serializable
-    :param csrf: Whether to include a csrf double submit claim in this token
-                 (boolean)
     :param identity_claim_key: Which key should be used to store the identity
     :param user_claims_key: Which key should be used to store the user claims
+    :param json_encoder: json encoder
     :return: Encoded refresh token
     """
     token_data = {
@@ -81,8 +103,8 @@ async def encode_refresh_token(identity, secret, algorithm, expires_delta, user_
                        json_encoder=json_encoder)
 
 
-async def decode_jwt(encoded_token, secret, algorithm, identity_claim_key,
-                     user_claims_key):
+async def decode_jwt(encoded_token: str, secret: str, algorithm: str, identity_claim_key: str,
+                     user_claims_key: str) -> Dict:
     """
     Decodes an encoded JWT
 
@@ -94,7 +116,7 @@ async def decode_jwt(encoded_token, secret, algorithm, identity_claim_key,
     :return: Dictionary containing contents of the JWT
     """
     # This call verifies the ext, iat, and nbf claims
-    data = jwt.decode(encoded_token, secret, algorithms=[algorithm])
+    data: dict = jwt.decode(encoded_token, secret, algorithms=[algorithm])
 
     # Make sure that any custom claims we expect in the token are present
     if 'jti' not in data:
@@ -113,20 +135,33 @@ async def decode_jwt(encoded_token, secret, algorithm, identity_claim_key,
 
 
 class Token:
+    """
+    Token object that contains decoded token data and passed with kwargs to endpoint function
+    """
     data: dict
+    app: Sanic
 
-    def __init__(self, app, token: dict):
+    def __init__(self, app: Sanic, token: dict):
         self.app = app
         self.data = token
 
     @property
-    def raw_jwt(self):
+    def raw_jwt(self) -> dict:
+        """
+        :return: full jwt data in dictionary form
+        """
         return self.data
 
     @property
-    def jwt_identity(self):
+    def jwt_identity(self) -> Union[str, None]:
+        """
+        :return: jwt identity claim data (or this can be None if data does not exist)
+        """
         return self.data.get(self.app.config.JWT_IDENTITY_CLAIM, None)
 
     @property
-    def jwt_claims(self):
+    def jwt_claims(self) -> Dict:
+        """
+        :return: user claim data
+        """
         return self.data.get(self.app.config.JWT_USER_CLAIMS, {})

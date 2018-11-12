1f88c6d4725b947eff43d61d51ec425c8bf92573
Lewis <contact@artoria.us>
2018-11-12 19:51:31 +0900

    REMOVE: useless import

--- sanic_jwt_extended/__init__.py
+++ sanic_jwt_extended/__init__.py
@@ -1,7 +1,5 @@
 from .jwt_manager import (JWTManager)
-from .exceptions import *
-from .utils import *
-from .decorators import *
-from .tokens import *
+from .utils import (create_refresh_token, create_access_token)
+from .decorators import (jwt_required, jwt_optional, jwt_refresh_token_required, fresh_jwt_required)
 
 __version__ = "0.0.1"

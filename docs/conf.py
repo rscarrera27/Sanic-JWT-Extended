ed5e5bd994fd69706905fcf4b8ea1f1012b23b82
Lewis <contact@artoria.us>
2018-11-13 08:38:57 +0900

    ADD: mock import -> but... why?

--- docs/conf.py
+++ docs/conf.py
@@ -292,3 +292,4 @@ texinfo_documents = [
 
 # Example configuration for intersphinx: refer to the Python standard library.
 intersphinx_mapping = {'https://docs.python.org/': None}
+autodoc_mock_imports = ["flask_jwt_extended"]

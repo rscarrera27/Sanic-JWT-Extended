109d99c5b1155a845da86743a88b365cb76f0439
Seonghyeon Kim <artoria@artoria.us>
2019-07-06 17:26:52 +0900

    RELEASE: 0.3.1

--- sanic_jwt_extended/__init__.py
+++ sanic_jwt_extended/__init__.py
@@ -2,4 +2,4 @@ from .jwt_manager import (JWTManager)
 from .utils import (create_refresh_token, create_access_token)
 from .decorators import (jwt_required, jwt_optional, jwt_refresh_token_required, fresh_jwt_required)
 
-__version__ = "0.2.0"
+__version__ = "0.3.1"

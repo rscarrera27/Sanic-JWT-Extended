830cd356bc395ae435145bbc9e008e9ebba5ad17
NeverMine17 <dannevergame@gmail.com>
2019-07-10 16:20:47 +0300

    Added documentation

--- sanic_jwt_extended/blacklists.py
+++ sanic_jwt_extended/blacklists.py
@@ -6,23 +6,48 @@ from datetime import datetime, timedelta
 class Blacklist(abc.ABC):
     @abc.abstractmethod
     async def get_variable(self, name: str) -> str:
+        """
+        Abstract method that gets some kind of variable with a given name.
+        If variable with a given name does not exist, return None.
+        MUST not throw any exceptions.
+        :param name: name of the variable
+        :return: value of the variable
+        """
         pass
 
     @abc.abstractmethod
     async def set_variable(self, name: str, value: str):
+        """
+        Abstract method that sets some kind of variable with a given name a given value.
+        MUST not throw any exceptions.
+        :param name: name of the variable
+        :param value: value of the variable
+        """
         pass
 
-    async def check_token(self, jwt_id: str):
+    async def check_token(self, jwt_id: str) -> bool:
+        """
+        Checks if the token is not banned at the moment.
+        MUST not throw any exceptions.
+        :param jwt_id: Some kind of id that's
+        unique to the token. For example jti (https://tools.ietf.org/html/rfc7519#section-4.1.7)
+        """
         return (
             await self.get_variable(jwt_id) == "true"
             and datetime.fromisoformat(await self.get_variable(jwt_id + "_date"))
             > datetime.now()
         )
 
-    async def blacklist_token(self, jwt_id: str, ban_for: timedelta):
+    async def blacklist_token(self, jwt_id: str, blacklist_for: timedelta):
+        """
+        Blacklists token for a given time
+        :param jwt_id: Some kind of id that's
+        unique to the token. For example jti (https://tools.ietf.org/html/rfc7519#section-4.1.7)
+        :param blacklist_for: For how much time should this token be banned
+        """
         await self.set_variable(jwt_id, "true")
         await self.set_variable(
-            jwt_id + "_date", (datetime.now() + ban_for).isoformat()
+            jwt_id + "_date", (datetime.now() + blacklist_for).isoformat()
         )
 
 
